ebe1339d18b118cf0aec6e806fb970f87f126128
elkmeat12 <38544362+elkmeat12@users.noreply.github.com>
2020-01-16 15:47:59 -0700

    Added class exercise

---
+++ web/week2/inclass/practice.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Class Exercise</title>
+
+   <style> 
+      div:nth-child(even) {
+         color: red;
+      }
+   </style>
+</head>
+<body>
+   <?php
+      echo "<h2>PHP Team Activity</h2>";
+
+      $count = 0;
+
+      while($count <= 10) {
+         echo "<div id='div$count'>This is div #$count</div>";
+         ++$count;
+      }
+   ?>
+</body>
+</html>
