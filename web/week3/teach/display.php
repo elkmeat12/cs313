b36975ee9c23c0f0721296a9f8b4ee2c2887b0b4
elkmeat12 <38544362+elkmeat12@users.noreply.github.com>
2020-01-21 15:00:07 -0700

    Updated email

--- web/week3/teach/display.php
+++ web/week3/teach/display.php
@@ -1,7 +1,7 @@
 <html>
    <body>
       Name: <?php echo $_POST["name"]; ?><br>
-      Email: <?php echo "<a href='mailto:'" . $_POST['email'] . ">"; ?><br>
+      Email: <?php echo "<a href='mailto:" . $_POST['email'] . "'>"; ?><br>
       Major: <?php echo $_POST["major"]; ?><br>
       Comments: <?php echo $_POST["comment"]; ?><br>
    </body>
