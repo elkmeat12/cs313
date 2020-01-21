99f0754ff53766dd8b9253a4b33cf2ba87545863
elkmeat12 <38544362+elkmeat12@users.noreply.github.com>
2020-01-21 15:48:06 -0700

    Added continents

--- web/week3/teach/display.php
+++ web/week3/teach/display.php
@@ -5,11 +5,24 @@
          $email = htmlspecialchars($_POST["email"]);
          $major = htmlspecialchars($_POST["major"]);
          $comment = htmlspecialchars($_POST["comment"]);
+         $continent = $_POST["continent"];
 
          echo "Name: $name <br>";
          echo "Email: <a href='mailto:$email'>$email</a><br>";
          echo "Major: $major<br>";
          echo "Comments: $comment";
+
+         if (empty($continent))
+         {
+            echo "You should get out of the house...";
+         }
+         else
+         {
+            for ($i = 0; $i < count($continent); $i++)
+            {
+               echo $continent[$i] . "<br>";
+            }
+         }
       ?>
    </body>
 </html>
