71733c28949967ef6a69e48226ed0fc0598ecd50
elkmeat12 <38544362+elkmeat12@users.noreply.github.com>
2020-01-21 14:57:43 -0700

    Added week 3 team assignment

---
+++ web/week3/teach/display.php
@@ -0,0 +1,8 @@
+<html>
+   <body>
+      Name: <?php echo $_POST["name"]; ?><br>
+      Email: <?php echo "<a href='mailto:'" . $_POST['email'] . ">"; ?><br>
+      Major: <?php echo $_POST["major"]; ?><br>
+      Comments: <?php echo $_POST["comment"]; ?><br>
+   </body>
+</html>
