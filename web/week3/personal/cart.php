7161c0f78e0dc5d565c7cacfdcc37f98d592050f
elkmeat12 <38544362+elkmeat12@users.noreply.github.com>
2020-01-23 19:51:29 -0700

    Removed button

--- web/week3/personal/cart.php
+++ web/week3/personal/cart.php
@@ -237,7 +237,7 @@
   <!-- /.container -->
   <br><br>
   <!-- Footer -->
-  <footer class="py-5 bg1 fixed-bottom">
+  <footer class="py-5 bg1">
     <div class="container">
       <p class="m-0 text-center text-white">Copyright &copy; The Snack Shop</p>
     </div>
