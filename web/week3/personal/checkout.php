45a8f7aac6e636457c9e9e6d5621acf1f87e7e28
elkmeat12 <38544362+elkmeat12@users.noreply.github.com>
2020-01-23 19:40:54 -0700

    Added checkout PHP

---
+++ web/week3/personal/checkout.php
@@ -0,0 +1,99 @@
+<?php
+   session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>The Snack Shop</title>
+
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  
+  <!-- Custom styles for this template -->
+  <link href="prove3.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script type="text/javascript" src="prove3.js"></script>
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg1">
+    <div class="container">
+      <a class="navbar-brand" href="prove3.php">The Snack Shop</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="prove3.php">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="cart.php">View Cart</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Content -->
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-lg-3">
+
+        <!-- <h2 class="my-4">Browse Snacks</h2> -->
+        <div class="list-group my-4">
+          <a href="prove3.php" class="list-group-item">Back to Browse</a>
+          <a href="cart.php" class="list-group-item">Return to Cart</a>
+        </div>
+
+      </div>
+      <!-- /.col-lg-3 -->
+
+      <div class="col-lg-9">
+
+        <h4 class="my-4">Order Information</h4>
+
+        <hr>
+
+        <div class="row">
+
+          
+
+        </div>
+        <!-- /.row -->
+        <button onclick="window.location.href = 'confirm.php';" class="btn btn-primary">Order</button>
+      </div>
+      <!-- /.col-lg-9 -->
+
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+  <br><br>
+  <!-- Footer -->
+  <footer class="py-5 bg1 fixed-bottom">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; The Snack Shop</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+
+</body>
+</html>
