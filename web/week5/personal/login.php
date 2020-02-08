5a9f00b0de62ea21bef883560b04f06d8bed9dd4
elkmeat12 <38544362+elkmeat12@users.noreply.github.com>
2020-02-07 19:44:34 -0700

    Added login page

---
+++ web/week5/personal/login.php
@@ -0,0 +1,104 @@
+<?php
+   session_start();
+   require "../../temp/dbConnect.php";
+   $db = get_db();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Hooked :: Order Receipt</title>
+
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  
+  <!-- Custom styles for this template -->
+  <link href="login.css" rel="stylesheet">
+  <link href="hooked.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script type="text/javascript" src="hooked.js"></script>
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg1">
+    <div class="container">
+      <a class="navbar-brand" href="hooked.php">Hooked</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="hooked.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="login.php">Admin</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="cart.php">View Cart</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Content -->
+  <div class="container">
+
+    <div class="row">
+
+      <!-- /.col-lg-3 -->
+
+      <div class="col-lg-9">
+
+        <h4 class="my-4">Delivery Order</h4>
+
+        <hr>
+         <form action="admin.php" method="post">
+            <div class="container">
+               <label for="uname"><b>Username</b></label>
+               <input type="text" placeholder="Enter Username" name="uname" required>
+
+               <label for="psw"><b>Password</b></label>
+               <input type="password" placeholder="Enter Password" name="psw" required>
+
+               <button type="submit">Login</button>
+               <label>
+                  <input type="checkbox" checked="checked" name="remember"> Remember me
+               </label>
+            </div>
+
+         </form>
+
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.col-lg-9 -->
+
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+  <br><br>
+  <!-- Footer -->
+  <footer class="py-5 bg1">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Hooked</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+
+</body>
+</html>
