b0e6b464e418b23f9eaafc9aaadd118e242649e2
elkmeat12 <38544362+elkmeat12@users.noreply.github.com>
2020-02-20 15:13:59 -0700

    Adding login styles

--- web/week7/signIn.php
+++ web/week7/signIn.php
@@ -67,12 +67,11 @@
                               <input type="password" name="txtPass" id="txtPass" class="form-control" maxlength="16">
                            </div>
                            <div class="form-group">
-                              <input type="submit" name="submit" class="btn btn-info btn-md" value="Submit">
-                              <a href="signUp.php" class="text-info text-right float-right">Create New Account</a>
+                              <input type="submit" name="submit" class="btn btn-info btn-md center-item" value="Login">
                            </div>
-                           <!-- <div id="register-link" class="text-right">
-                              <a href="signUp.php" class="text-info">Create New Account Here</a>
-                           </div> -->
+                           <div class="form-group">
+                              <a href="signUp.php" class="text-info text-right float-right center-item">Create New Account</a>
+                           </div>
                      </form>
                   </div>
                </div>
